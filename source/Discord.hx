bc34f92f4ee1e635004cd5a38e8cd886d6c1aced
ppalionelgg <camilocarlos1100@gmail.com>
2024-09-09 17:40:43 -0500

    :trollface:

--- source/Discord.hx
+++ source/Discord.hx
@@ -1,6 +1,6 @@
 package;
 
-#if windows
+//#if windows
 import Sys.sleep;
 import discord_rpc.DiscordRpc;
 
@@ -86,4 +86,4 @@ class DiscordClient
 		//trace('Discord RPC Updated. Arguments: $details, $state, $smallImageKey, $hasStartTimestamp, $endTimestamp');
 	}
 }
-#end
+//#end
